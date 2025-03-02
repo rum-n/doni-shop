28b274f62d50b3004f17ff237de2788185fee067
rum-n <rumen.mnv@protonmail.com>
2025-03-02 22:42:42 +0200

    fix build

--- src/app/api/artwork/[slug]/route.ts
+++ src/app/api/artwork/[slug]/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@/lib/db';
 
-export async function GET(
-  req: NextRequest, res: NextResponse
-) {
+export async function GET(req: NextRequest) {
   try {
     const slug = req.nextUrl.searchParams.get('slug');
 

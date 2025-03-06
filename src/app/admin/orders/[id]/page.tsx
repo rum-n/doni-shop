ca9c24d2aaa500d465faaac9876cfb3a0fa52da0
rum-n <rumen.mnv@protonmail.com>
2025-03-06 12:05:30 +0200

    remove import

--- src/app/admin/orders/[id]/page.tsx
+++ src/app/admin/orders/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { redirect, useParams } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
